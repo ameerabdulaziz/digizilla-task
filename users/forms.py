bd17e13d95e657d60688dfa30d1cc1533bd1d4c2
Ameer Abdulaziz <ameer@idealisticsolutins.com>
2019-02-27 11:21:00 +0200

    Create registration form

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,17 @@
-from django.contrib.auth.forms import PasswordResetForm
+from django import forms
+from django.contrib.auth.forms import PasswordResetForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+
+
+class UserRegisterForm(UserCreationForm, forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.TextInput)
+    last_name = forms.CharField(required=True, widget=forms.TextInput)
+    email = forms.CharField(required=True, widget=forms.EmailInput)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
 
 
 class EmailValidationOnForgotPassword(PasswordResetForm):
--- users/views.py
+++ users/views.py
@@ -1,13 +1,15 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (
-    LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView, auth_logout
+    LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
     )
 from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 
-from users.forms import EmailValidationOnForgotPassword
+from users.forms import EmailValidationOnForgotPassword, UserRegisterForm
 
 
 class Login(LoginView):
@@ -17,7 +19,6 @@ class Login(LoginView):
     def form_valid(self, form):
         super(Login, self).form_valid(form)
         self.request.session['user_logged_in'] = self.request.POST.get('username')
-        print(self.request.session['user_logged_in'])
         if self.request.is_ajax():
             data = {
                 'valid': True
@@ -34,6 +35,26 @@ class Login(LoginView):
             }
             return JsonResponse(data)
         return super().form_invalid(form)
+
+
+class Register(CreateView):
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        self.request.session['user_logged_in'] = username
+        return super().form_valid(form)
+
+    def get_template_names(self):
+        if 'user_logged_in' in self.request.session:
+            raise PermissionDenied
+        else:
+            return 'users/register.html'
 
 
 class LoginSuccess(LoginRequiredMixin, TemplateView):
